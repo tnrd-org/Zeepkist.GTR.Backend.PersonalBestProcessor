cf77da1d4a44cbdc5c0b90b571a1bd6257ed4597
Christiaan Bloemendaal <github@tnrd.net>
2023-05-29 21:31:58 +0200

    feat: add publishing record on change

---
+++ Rabbit/IRabbitPublisher.cs
@@ -0,0 +1,9 @@
+﻿using RabbitMQ.Client;
+
+namespace TNRD.Zeepkist.GTR.Backend.PersonalBestProcessor.Rabbit;
+
+public interface IRabbitPublisher
+{
+    void Initialize(IModel channel);
+    void Publish(string exchange, object data);
+}
