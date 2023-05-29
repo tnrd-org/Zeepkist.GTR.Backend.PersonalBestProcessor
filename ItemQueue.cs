9a6c706684e99e33dc556ec5e4b0c3308bce1a56
Christiaan Bloemendaal <github@tnrd.net>
2023-05-29 14:15:40 +0200

    feat: initial version

---
+++ ItemQueue.cs
@@ -0,0 +1,38 @@
+﻿using FluentResults;
+using TNRD.Zeepkist.GTR.DTOs.Rabbit;
+
+namespace TNRD.Zeepkist.GTR.Backend.PersonalBestProcessor;
+
+internal class ItemQueue
+{
+    private readonly AutoResetEvent resetEvent = new(true);
+    private readonly Dictionary<int, List<ProcessPersonalBestRequest>> userToItems = new();
+
+    public bool HasItems()
+    {
+        return userToItems.Values.Any(x => x.Count > 0);
+    }
+
+    public void AddToQueue(ProcessPersonalBestRequest item)
+    {
+        resetEvent.WaitOne();
+
+        if (!userToItems.ContainsKey(item.User))
+            userToItems.Add(item.User, new List<ProcessPersonalBestRequest>());
+
+        userToItems[item.Level].Add(item);
+
+        resetEvent.Set();
+    }
+
+    public Dictionary<int, List<ProcessPersonalBestRequest>> GetItemsFromQueue()
+    {
+        resetEvent.WaitOne();
+
+        Dictionary<int, List<ProcessPersonalBestRequest>> copy = userToItems.ToDictionary(x => x.Key, y => y.Value);
+        userToItems.Clear();
+
+        resetEvent.Set();
+        return copy;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Serilog;
+using TNRD.Zeepkist.GTR.Backend.PersonalBestProcessor;
+using TNRD.Zeepkist.GTR.Backend.PersonalBestProcessor.Rabbit;
+using TNRD.Zeepkist.GTR.Database;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .UseSerilog((context, configuration) =>
+    {
+        configuration
+            .MinimumLevel.Debug()
+            .WriteTo.Console();
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<QueueProcessor>();
+
+        services.Configure<RabbitOptions>(context.Configuration.GetSection("Rabbit"));
+        services.AddHostedService<RabbitWorker>();
+
+        services.AddNpgsql<GTRContext>(context.Configuration["Database:ConnectionString"]);
+
+        services.AddSingleton<ItemQueue>();
+    })
+    .Build();
+
+host.Run();
