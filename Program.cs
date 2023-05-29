68d88dd17799c4ac7d1858569600753e9dfc4cdf
Christiaan Bloemendaal <github@tnrd.net>
2023-05-29 15:36:25 +0200

    feat: add extra exception logging

--- Program.cs
+++ Program.cs
@@ -3,24 +3,42 @@ using TNRD.Zeepkist.GTR.Backend.PersonalBestProcessor;
 using TNRD.Zeepkist.GTR.Backend.PersonalBestProcessor.Rabbit;
 using TNRD.Zeepkist.GTR.Database;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .UseSerilog((context, configuration) =>
+internal class Program
+{
+    public static void Main(string[] args)
     {
-        configuration
-            .MinimumLevel.Verbose()
-            .WriteTo.Console();
-    })
-    .ConfigureServices((context, services) =>
-    {
-        services.AddHostedService<QueueProcessor>();
+        IHost host = Host.CreateDefaultBuilder(args)
+            .UseSerilog((context, configuration) =>
+            {
+                configuration
+                    .MinimumLevel.Verbose()
+                    .WriteTo.Console();
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<QueueProcessor>();
 
-        services.Configure<RabbitOptions>(context.Configuration.GetSection("Rabbit"));
-        services.AddHostedService<RabbitWorker>();
+                services.Configure<RabbitOptions>(context.Configuration.GetSection("Rabbit"));
+                services.AddHostedService<RabbitWorker>();
 
-        services.AddNpgsql<GTRContext>(context.Configuration["Database:ConnectionString"]);
+                services.AddNpgsql<GTRContext>(context.Configuration["Database:ConnectionString"]);
 
-        services.AddSingleton<ItemQueue>();
-    })
-    .Build();
+                services.AddSingleton<ItemQueue>();
+            })
+            .Build();
 
-host.Run();
+        ILogger<Program> logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+        TaskScheduler.UnobservedTaskException += (sender, eventArgs) =>
+        {
+            logger.LogError(eventArgs.Exception, "Unobserved task exception");
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
+        {
+            logger.LogError(eventArgs.ExceptionObject as Exception, "Unhandled exception");
+        };
+
+        host.Run();
+    }
+}
